32ad20e8c75958a4611869bab61c35cf8c8fa55c
Moonbyeongchan123 <ansqudcks97@gamail.com>
2020-09-18 12:05:06 +0900

    Edit 03장

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+function Food(porps) {
+  return( 
+    <h1>I like {porps.fav}</h1>
+  )
+}
+
 function App() {
-  // return <div className="App" />;
-  return <div>
-    <h1>Hello!!!!!</h1>
-    </div>;
+    return (
+      <div>
+        <h1>Hello</h1>  
+        <Food fav="김치" />
+        <Food fav="짜장면" />
+        <Food fav="짬뽕" />
+        <Food fav="만두" />
+        <Food fav="피자" />
+      </div>  
+    );
 }
 
 export default App;
