58c48f64dce0df24330dd1210970cb0d0325bd05
Moonbyeongchan123 <ansqudcks97@gamail.com>
2020-09-18 12:46:43 +0900

    9월 18일

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
-function Food(porps) {
-  return( 
-    <h1>I like {porps.fav}</h1>
+function Food({fav}) {
+  return ( 
+    <h1>I like {fav}</h1>
   )
 }
+
+const foodLike = [
+  {
+      name: '김치', 
+  },
+  {
+      name: '만두',
+  },
+  {
+      name: '피자',
+  },  
+];
 
 function App() {
     return (
       <div>
         <h1>Hello</h1>  
-        <Food fav="김치" />
-        <Food fav="짜장면" />
-        <Food fav="짬뽕" />
-        <Food fav="만두" />
-        <Food fav="피자" />
       </div>  
     );
 }
